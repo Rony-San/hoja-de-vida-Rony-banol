d864b650e48a5162c3819378a5e00aee7ad94ac1
Rony Santiago Bañol <rony.banolueda.edu.co>
2022-01-25 08:56:03 -0500

    probando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,18 @@ import foto from 'images/foto.png';
 
 const Header = () => (
   <header className='bg-Negro-plantilla flex items-center h-1/2 md:hidden text-white'>
-    <div className='bg-Negro-plantilla h-4/5 flex flex-col items-center justify-self-center p-3 pt-5 w-screen'>
+    <div className='bg-Negro-plantilla h-4/5 flex flex-col items-center justify-self-center p-3 pt-5 w-screen '>
       <div className='flex items-center mx-20  '>
         <img src={foto} alt='nada' className='w-20 h-20 rounded-full ml-1 ' />
-        <div className=' w-80 flex flex-col ml-10'>
+        <div className=' w-80 flex flex-col ml-2'>
           <h1 className=' font-bold text-lg border-b-2 w-fit'>
-            Rony Santiago Bañol Ricox
+            Rony Santiago Bañol Rico
           </h1>
-          <p className=' mt-2 '>
-            Estudiante ingeniería <br /> de sistemas
-          </p>
+          <p className=' mt-2 '>Estudiante ingeniería de sistemas</p>
         </div>
       </div>
 
-      <p className=' mx-auto  w-3/4 mt-2 '>
+      <p className=' mx-auto  w-11/12 mt-2 text-xs '>
         Estudiante de Ingeniería de Sistemas con gran pasión por el desarrollo y
         los nuevos retos en camino de crecimiento profesional; con gran
         capacidad de autoaprendizaje y trabajo en equipo.
