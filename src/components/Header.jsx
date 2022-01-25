ac62d359ba7c60ce5cf43108387153ab5791541a
Rony Santiago Bañol <rony.banolueda.edu.co>
2022-01-25 08:34:33 -0500

    probando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import foto from 'images/foto.png';
 
 const Header = () => (
   <header className='bg-Negro-plantilla flex items-center h-1/2 md:hidden text-white'>
-    <div className='bg-Negro-plantilla h-4/5 flex flex-col items-center justify-self-center p-3 pt-5'>
+    <div className='bg-Negro-plantilla h-4/5 flex flex-col items-center justify-self-center p-3 pt-5 w-screen'>
       <div className='flex items-center mx-20  '>
         <img src={foto} alt='nada' className='w-20 h-20 rounded-full ml-1 ' />
         <div className=' w-80 flex flex-col ml-10'>
           <h1 className=' font-bold text-lg border-b-2 w-fit'>
-            Rony Santiago Bañol Rico
+            Rony Santiago Bañol Ricox
           </h1>
           <p className=' mt-2 '>
             Estudiante ingeniería <br /> de sistemas
